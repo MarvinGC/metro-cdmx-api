256f0dd052a3835d41cc75e0e2bee96dae61922a
MarvinGC <marlexgscl@gmail.com>
2022-10-31 11:24:41 -0600

    Spring initializer

---
+++ src/main/java/com/marvingcdev/Metro/CDMX/MetroCdmxApplication.java
@@ -0,0 +1,13 @@
+package com.marvingcdev.Metro.CDMX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroCdmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroCdmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvingcdev/Metro/CDMX/MetroCdmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvingcdev.Metro.CDMX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroCdmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
